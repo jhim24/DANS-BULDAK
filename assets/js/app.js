626b64a7cd7939b9bdc96bb009535b5fc46c428b
jhim24 <85859168+jhim24@users.noreply.github.com>
2026-07-26 17:11:57 +0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -244,3 +244,42 @@ console.log(err);
 });
 
 }
+/*=========================
+HERO SLIDER
+=========================*/
+
+const slides = document.querySelectorAll(".hero-slide");
+const dots = document.querySelectorAll(".dot");
+
+let currentSlide = 0;
+
+function showSlide(index){
+
+    slides.forEach((slide)=>{
+        slide.classList.remove("active");
+    });
+
+    dots.forEach((dot)=>{
+        dot.classList.remove("active");
+    });
+
+    slides[index].classList.add("active");
+    dots[index].classList.add("active");
+
+}
+
+function nextSlide(){
+
+    currentSlide++;
+
+    if(currentSlide >= slides.length){
+
+        currentSlide = 0;
+
+    }
+
+    showSlide(currentSlide);
+
+}
+
+setInterval(nextSlide,4000);
